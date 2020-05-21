ccbfbb382181fe1fa45c114226f18543126c2789
Daniel Morales <mralejo99@gmail.com>
2020-05-21 15:45:30 -0500

    Angular modified

--- fronted/README.md
+++ fronted/README.md
@@ -1,6 +1,6 @@
 # Fronted
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
 ## Development server
 
--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -1,51 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {FormsModule} from '@angular/forms'
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
-
-import { AppComponent } from './app.component';
-import { EmployeesComponent } from './components/employees/employees.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SignupComponent } from './components/signup/signup.component';
-import { SigninComponent } from './components/signin/signin.component';
-import { GamesComponent } from './components/games/games.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { HomeComponent } from './components/home/home.component';
-import { ConsolasComponent } from './components/consolas/consolas.component';
-import { PrivateGamesComponent } from './components/private-games/private-games.component';
-import { UsersComponent } from './components/users/users.component';
-
-import { AuthGuard } from "./auth.guard";
-import { TokenInterceptorService } from "./services/token-interceptor.service";
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeesComponent,
-    SignupComponent,
-    SigninComponent,
-    GamesComponent,
-    ProfileComponent,
-    HomeComponent,
-    ConsolasComponent,
-    PrivateGamesComponent,
-    UsersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [
-    AuthGuard,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass:TokenInterceptorService,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
