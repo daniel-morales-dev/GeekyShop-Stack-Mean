152aca441a17e0df6d6d3daf603e65e044afa9b6
Daniel Morales <mralejo99@gmail.com>
2020-06-29 20:44:04 -0500

    Heroku server

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.use(cors({ origin: 'http://localhost:4200' })); //Uno el backend con el fron
 //Routes
 app.use(require('./routes/index')); //Importo las rutas que manejara el servidor
 app.use('/public', express.static(__dirname + '/public/uploads/img'));
-app.use(express.static(__dirname + 'dist/fronted'));
+app.use(express.static(__dirname + '/dist/fronted'));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + 'dist/fronted/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/fronted/index.html'));
 });
 
 //Starting server
